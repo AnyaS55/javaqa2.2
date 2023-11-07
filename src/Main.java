8b2932ead18fe73a93b4ee77b7e89dde2b8084b1
Anya <annasedova123456@gmail.com>
2023-11-07 20:54:09 +0300

    secomd commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main (String [] args) {
+
+        int balance = 100;
+        int cash = 1100;
+        int bonus = 1;
+
+        if (cash > 1000) {
+            balance = (balance + cash+ cash/100 * bonus);
+            System.out.println(balance);
+        } else {
+            balance = ( balance + cash);
+            System.out.println(balance);
+        }
+
+
+    }
+
+
 }
