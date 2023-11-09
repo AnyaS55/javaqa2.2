24f797b07c3ded80bc97149a3d3a11bc5b084e24
Anya <annasedova123456@gmail.com>
2023-11-09 19:48:48 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         int balance = 100;
-        int cash = 1100;
+        int cash = 1;
         int bonus = 1;
 
         if (cash > 1000) {
-            balance = (balance + cash+ cash/100 * bonus);
+            balance = (balance + cash + cash / 100 * bonus);
             System.out.println(balance);
         } else {
-            balance = ( balance + cash);
+            balance = (balance + cash);
             System.out.println(balance);
         }
 
